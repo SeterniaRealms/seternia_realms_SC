6ead1ac744421bbeeaaf21a822ef80fcdc1b230a
Catripilar <70659123+catripilar@users.noreply.github.com>
2024-06-07 15:15:14 +0200

    Add files via upload

---
+++ tests/require.ts
@@ -0,0 +1,62 @@
+import BN from "bn.js";
+import { PublicKey } from '@solana/web3.js';
+
+export interface Treasure {
+  admin: PublicKey;
+  mainCollection: PublicKey;
+  runeCollection: PublicKey;
+}
+
+export interface Role {
+  addresses: PublicKey[];
+}
+
+export interface CoinWallet {
+  owner: PublicKey;
+  amount: BN;
+}
+
+export interface Faction {
+  id: BN;
+  season: BN;
+  coins: BN;
+  members: BN;
+  distribution: BN;
+  classes: BN;
+  faction: string;
+}
+
+export interface Class {
+  faction: PublicKey;
+  title: string;
+  symbol: string;
+  id: BN;
+  traits: BN;
+}
+export interface Race {
+  value: BN;
+  token: PublicKey;
+  class: PublicKey;
+  limit: BN;
+  current: BN;
+  name: string;
+}
+export interface Traits {
+  race: PublicKey;
+  level: BN;
+  coins: BN;
+  uri: string;
+}
+
+export interface MintData {
+  attributes: PublicKey;
+  race: PublicKey;
+  traits: PublicKey;
+  mint: PublicKey;
+  season: BN;
+  level: BN;
+}
+
+export interface ClassList {
+  factionData: PublicKey[];
+}
